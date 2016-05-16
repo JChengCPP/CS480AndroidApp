2e954114394da754cd01ba3cae62917c1aaa5a8b
Jimmy Cheng <jimmycheng@cpp.edu>
2016-05-15 22:33:17 -0700

    commit full directory

--- app/src/main/java/cs480teamavatar/cs480androidapp/LoginPage.java
+++ app/src/main/java/cs480teamavatar/cs480androidapp/LoginPage.java
@@ -2,6 +2,7 @@ package cs480teamavatar.cs480androidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        String username = ((EditText) findViewById(R.id.login_text)).toString();
+        String password = ((EditText) findViewById(R.id.login_password_text)).toString();
+
     }
 }
--- app/src/main/java/cs480teamavatar/cs480androidapp/StartUp.java
+++ app/src/main/java/cs480teamavatar/cs480androidapp/StartUp.java
@@ -17,6 +17,7 @@ public class StartUp extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(StartUp.this, LoginPage.class);
+                startActivity(intent);
             }
         });
     }
